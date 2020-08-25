7f8223b29e4c2525ac9b3a01138dcb0d446d277c
yeraidm <yerai.doval@uvigo.es>
2020-08-25 17:51:22 +0200

    Removed unnecessary computations and dependencies.

--- experiments/dictionary_induction/test.py
+++ experiments/dictionary_induction/test.py
@@ -10,8 +10,6 @@ hits1 = 0
 hits5 = 0
 hits10 = 0
 n = 0
-crit1 = torch.dot
-crit2 = torch.nn.MSELoss()
 
 for i, line in enumerate(sys.stdin):
     words = line.strip().split(" ")
@@ -19,14 +17,10 @@ for i, line in enumerate(sys.stdin):
         sys.stderr.write("Number of models and words not matching at line " + str(i) + "!\n")
         continue
     try:
-        crit1_sum += crit1(*[torch.autograd.Variable(torch.Tensor(x)) / torch.autograd.Variable(torch.Tensor(x)).norm() 
-            for x in [model[word] for word, model in zip(words, models)]])
-        crit2_sum += crit2(*[torch.autograd.Variable(torch.Tensor(x))
-            for x in [model[word] for word, model in zip(words, models)]])
         emb = models[0][words[0]]
-        nn1 = set([models[-1].similar_by_vector(np.array(emb))[0][0]])
-        nn5 = set(x[0] for x in models[-1].similar_by_vector(np.array(emb))[:5])
-        nn10 = set(x[0] for x in models[-1].similar_by_vector(np.array(emb)))
+        nn10 = [x[0] for x in models[-1].similar_by_vector(np.array(emb))]
+        nn5 = nn10[:5]
+        nn1 = [nn10[0]]
         if words[-1] in nn1:
             hits1 += 1
             # sys.stderr.write("FOUND: " + words[-1] + " in NN1: " + " ".join(nn1) + "\n")
@@ -45,4 +39,4 @@ for i, line in enumerate(sys.stdin):
         n += 1
     except KeyError:
         continue
-print("MSE: " + str((crit2_sum/float(n)).data[0]) + " cos: " + str((crit1_sum/float(n)).data[0]) + " induction P@1: " + str(hits1/float(n)) + " induction P@5: " + str(hits5/float(n)) + " induction P@10: " + str(hits10/float(n)))
+print("induction P@1: " + str(hits1/float(n)) + " induction P@5: " + str(hits5/float(n)) + " induction P@10: " + str(hits10/float(n)))
